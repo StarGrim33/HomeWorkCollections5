3cdbe1514a260d94d35570ceee168bfd64a4578c
StarGrim33 <prosky33@gmail.com>
2022-11-26 12:35:16 +0300

    Работа с эффектами

---
+++ HomeWorkCollections5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HomeWorkCollections5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = { "1", "2", "1" };
+            string[] secondArray = { "3", "2" };
+
+            List<string> unitedCollection = new();
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+
+            }
+
+        }
+    }
+}
